d6002c95199259284ce973dc4009313d0821fafc
blee2125 <blee2125@gmail.com>
2020-12-01 13:52:45 -0500

    add input for zipcode

--- js-weather-app-frontend/src/index.js
+++ js-weather-app-frontend/src/index.js
@@ -4,3 +4,11 @@ const BACKEND_URL = 'localhost:3000';
 fetch(`http://${BACKEND_URL}/test`)
   .then(response => response.json())
   .then(parsedResponse => console.log(parsedResponse));
+
+function seeTheWeather() {
+  //gets zipcode from input
+  var zip = document.getElementById("zipcode").value
+
+  alert(`zipcode is ${zip}`);
+
+}
