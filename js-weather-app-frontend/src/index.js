dc5eb7dd2b3ab0a15b6294bbfad9666b8a033e88
blee2125 <blee2125@gmail.com>
2020-12-01 15:59:43 -0500

    connect openweathermap api

--- js-weather-app-frontend/src/index.js
+++ js-weather-app-frontend/src/index.js
@@ -7,9 +7,29 @@ fetch(`http://${BACKEND_URL}/test`)
 
 function seeTheWeather() {
   //gets zipcode from input
-  var zip = document.getElementById("zipcode").value
+  var zipcode = document.getElementById("zipcode").value
 
-  alert(`zipcode is ${zip}`);
+  //alert(`zipcode is ${zipcode}`);
+
+  let city = document.querySelector(".city")
+  let temperature = document.querySelector(".temperature")
+
+  if (zipcode) {
+    const api = `http://api.openweathermap.org/data/2.5/weather?zip=${zipcode},us&appid=8e96251acc0bcddc94b74de1c9fc5d22&units=imperial`
+
+    fetch(api)
+      .then(response =>{
+        return response.json()
+      })
+      .then(data =>{
+        console.log(data);
+        const cityName = data.name
+        const temperatureFloat = data.main.temp
+        const temperatureInt = Math.round(temperatureFloat)
+        city.textContent = cityName
+        temperature.textContent = temperatureInt + " °f"
+      })
+  }
 
 }
 
