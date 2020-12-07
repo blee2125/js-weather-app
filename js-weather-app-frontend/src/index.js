448cde78491d39d03d97b58c51953de0bf1d2c88
blee2125 <blee2125@gmail.com>
2020-12-07 14:00:20 -0500

    logIn and signUp work with backend

--- js-weather-app-frontend/src/index.js
+++ js-weather-app-frontend/src/index.js
@@ -1,49 +1,66 @@
-console.log("testing...123")
-// test that we can get data from the backend
-/*const BACKEND_URL = 'localhost:3000';
-fetch(`http://${BACKEND_URL}/test`)
-  .then(response => response.json())
-  .then(parsedResponse => console.log(parsedResponse));
-*/
-/*
-const cards = document.querySelector("body > div.cards")
-const newDiv = document.createElement("div")
-const newH3 = document.createElement("h3")
-const newH2 = document.createElement("h2")
-const newP = document.createElement("p")
+const BACKEND_URL = 'localhost:3000';
 
-cards.append(newDiv)
-newDiv.append(newH3)
-newDiv.append(newH2)
-newDiv.append(newP)
-newDiv.setAttribute("class", "card")
-newH3.setAttribute("class", "city")
-newH2.setAttribute("class", "temperature")
-newH2.innerHTML = "new card"
-newP.setAttribute("class", "description")
-*/
+let zipcodeArray = []
 
+
+
+function logIn() {
+  let username = document.getElementById("login").value
+  let password = document.getElementById("password").value
+
+  console.log({username: username, password: password})
+
+  fetch(`http://${BACKEND_URL}/login`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json",
+    },
+    body: JSON.stringify({username: username, password: password})
+  })
+    .then(response => response.json())
+    .then(parsedResponse => console.log(parsedResponse))
+}
+
+function signUp() {
+  let username = document.getElementById("login").value
+  let password = document.getElementById("password").value
+
+  console.log({username: username, password: password})
+
+  fetch(`http://${BACKEND_URL}/signup`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json",
+    },
+    body: JSON.stringify({username: username, password: password})
+  })
+    .then(response => response.json())
+    .then(parsedResponse => console.log(parsedResponse))
+}
 
 
 function seeTheWeather() {
   //gets zipcode from input
-  var zipcode = document.getElementById("zipcode").value
+  let zipcode = document.getElementById("zipcode").value
 
   //alert(`zipcode is ${zipcode}`);
 
-  let city = document.querySelector(".city")
-  let temperature = document.querySelector(".temperature")
-  let description = document.querySelector(".description")
-
   if (zipcode) {
     const api = `http://api.openweathermap.org/data/2.5/weather?zip=${zipcode},us&appid=8e96251acc0bcddc94b74de1c9fc5d22&units=imperial`
-
+    //zipcodeArray.unshift(zipcode)
+    //console.log(zipcodeArray)
     fetch(api)
       .then(response =>{
         return response.json()
       })
       .then(data =>{
-        console.log(data);
+        //console.log(data);
+        newCard();
+        let city = document.querySelector(".city")
+        let temperature = document.querySelector(".temperature")
+        let description = document.querySelector(".description")
         const cityName = data.name
         city.textContent = cityName
         const temperatureFloat = data.main.temp
@@ -51,19 +68,18 @@ function seeTheWeather() {
         temperature.textContent = temperatureInt + " °f"
         const descriptionApi = data.weather[0].description
         description.textContent = descriptionApi
-        newCard();
       })
   }
 }
 
 function newCard() {
-  const cards = document.querySelector("body > div.cards")
+  const newCards = document.querySelector("body > div.cards")
   const newDiv = document.createElement("div")
   const newH3 = document.createElement("h3")
   const newH2 = document.createElement("h2")
   const newP = document.createElement("p")
 
-  cards.append(newDiv)
+  newCards.append(newDiv)
   newDiv.append(newH3)
   newDiv.append(newH2)
   newDiv.append(newP)
@@ -81,5 +97,11 @@ document.getElementById("zipcode").addEventListener("keyup", function(event) {
   if(event.key == "Enter") {
     event.preventDefault();
     document.querySelector("button#zipcodeButton").click();
+  }
+});
+document.getElementById("password").addEventListener("keyup", function(event) {
+  if(event.key == "Enter") {
+    event.preventDefault();
+    document.querySelector("button#loginButton").click();
   }
 });
