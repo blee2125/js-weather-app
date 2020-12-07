ba22ea315709c9c457acb2080dc8dac4ca870f20
blee2125 <blee2125@gmail.com>
2020-12-07 13:58:33 -0500

    login/logout/signup routes

--- js-weather-app-backend/weather-app-api/config/routes.rb
+++ js-weather-app-backend/weather-app-api/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get '/test', to: 'application#test'
 
-  resources :sessions
-  
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  post '/signup', to: 'user#create'
 end
