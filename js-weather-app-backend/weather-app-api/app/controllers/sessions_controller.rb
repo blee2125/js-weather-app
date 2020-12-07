3c93485f989ba9b082f42657c93ab1777154b732
blee2125 <blee2125@gmail.com>
2020-12-07 13:57:36 -0500

    user can log in

--- js-weather-app-backend/weather-app-api/app/controllers/sessions_controller.rb
+++ js-weather-app-backend/weather-app-api/app/controllers/sessions_controller.rb
@@ -1,9 +1,19 @@
 class SessionsController < ApplicationController
+    require 'pry'
 
-    def set_current_user
-        if session[:user_id]
-            @current_user = User.find(session[:user_id])
+    def create
+        @user= User.find_by(name: params["session"]["username"])
+        if @user && @user.authenticate(params["session"]["password"])
+            session[:user_id] = @user.id
+            render json: {object: @user, message: "welcome back"}
+        else
+            render json: {message: "username/password incorrect"}
         end
+    end
+
+    def destroy
+        session.delete("user_id")
+        render json: {message: "Logged Out"}
     end
 
 end
