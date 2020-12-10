1ef2ce30f0ddf02dc84e4ead7e44c77c9f363082
blee2125 <blee2125@gmail.com>
2020-12-10 09:04:07 -0500

    move nonuser to app.js

--- js-weather-app-frontend/src/index.js
+++ js-weather-app-frontend/src/index.js
@@ -1,14 +1,8 @@
 const BACKEND_URL = 'localhost:3000';
 
-let zipcodeArray = []
-
-
-
 function logIn() {
   let username = document.getElementById("login").value
   let password = document.getElementById("password").value
-
-  console.log({username: username, password: password})
 
   fetch(`http://${BACKEND_URL}/login`, {
     method: "POST",
@@ -25,8 +19,6 @@ function logIn() {
 function signUp() {
   let username = document.getElementById("login").value
   let password = document.getElementById("password").value
-
-  console.log({username: username, password: password})
 
   fetch(`http://${BACKEND_URL}/signup`, {
     method: "POST",
@@ -45,38 +37,6 @@ function logOut() {
   fetch(`http://${BACKEND_URL}/logout`)
     .then(response => response.json())
     .then(parsedResponse => console.log(parsedResponse))
-}
-
-
-function seeTheWeather() {
-  //gets zipcode from input
-  let zipcode = document.getElementById("zipcode").value
-
-  //alert(`zipcode is ${zipcode}`);
-
-  if (zipcode) {
-    const api = `http://api.openweathermap.org/data/2.5/weather?zip=${zipcode},us&appid=8e96251acc0bcddc94b74de1c9fc5d22&units=imperial`
-    //zipcodeArray.unshift(zipcode)
-    //console.log(zipcodeArray)
-    fetch(api)
-      .then(response =>{
-        return response.json()
-      })
-      .then(data =>{
-        console.log(data)
-        let icon = document.querySelector(".icon")
-        let city = document.querySelector(".city")
-        let temperature = document.querySelector(".temperature")
-        let description = document.querySelector(".description")
-        const cityName = data.name
-        city.textContent = cityName
-        const temperatureFloat = data.main.temp
-        const temperatureInt = Math.round(temperatureFloat)
-        temperature.textContent = temperatureInt + " °f"
-        const descriptionApi = data.weather[0].description
-        description.textContent = descriptionApi
-      })
-  }
 }
 
 function newCard() {
