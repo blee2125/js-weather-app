f01cdd0d3f550a3466dd3964283cda3cc5cba763
Brandon Lee <blee2125@gmail.com>
2021-04-04 21:55:43 -0400

    add metric render

--- js-weather-app-frontend/src/app.js
+++ js-weather-app-frontend/src/app.js
@@ -28,49 +28,98 @@ class Weather{
   }
   
   renderWeather() {
-    let icon = document.querySelector("#icon")
-    const iconImage = this.icon
-    icon.setAttribute("src", `http://openweathermap.org/img/wn/${iconImage}@2x.png`)
+    if (settingsArray[1]=== "metric") {
+      let icon = document.querySelector("#icon")
+      const iconImage = this.icon
+      icon.setAttribute("src", `http://openweathermap.org/img/wn/${iconImage}@2x.png`)
 
-    let city = document.querySelector(".city")
-    const cityName = this.city
-    city.textContent = cityName
+      let city = document.querySelector(".city")
+      const cityName = this.city
+      city.textContent = cityName
 
-    let temperature = document.querySelector(".temperature")
-    const temperatureFloat = this.temperature
-    const temperatureInt = Math.round(temperatureFloat)
-    temperature.textContent = temperatureInt + " °f"
+      let temperature = document.querySelector(".temperature")
+      const temperatureFloat = this.temperature
+      const temperatureInt = Math.round(temperatureFloat)
+      temperature.textContent = temperatureInt + " °c"
 
-    const feelsLike = document.querySelector(".feels-like")
-    const feelsLikeFloat = this.feelsLike
-    const feelsLikeInt = Math.round(feelsLikeFloat)
-    feelsLike.textContent = "Feels Like: " + feelsLikeInt + " °f"
+      const feelsLike = document.querySelector(".feels-like")
+      const feelsLikeFloat = this.feelsLike
+      const feelsLikeInt = Math.round(feelsLikeFloat)
+      feelsLike.textContent = "Feels Like: " + feelsLikeInt + " °c"
 
-    const description = document.querySelector(".description")
-    const descriptionApi = this.description
-    description.textContent = descriptionApi
+      const description = document.querySelector(".description")
+      const descriptionApi = this.description
+      description.textContent = descriptionApi
 
-    let windDirection = document.querySelector(".wind-direction")
-    const windDirectionFloat = this.windDirection
-    windDirection.textContent = "Wind Direction: " + windDirectionFloat + "°"
+      let windDirection = document.querySelector(".wind-direction")
+      const windDirectionFloat = this.windDirection
+      windDirection.textContent = "Wind Direction: " + windDirectionFloat + "°"
 
-    let cardinalDirection = document.querySelector(".cardinal-direction")
-    const nsew = this.degToCompass(windDirectionFloat)
-    cardinalDirection.textContent = nsew
+      let cardinalDirection = document.querySelector(".cardinal-direction")
+      const nsew = this.degToCompass(windDirectionFloat)
+      cardinalDirection.textContent = nsew
 
-    let windSpeed = document.querySelector(".wind-speed")
-    const windSpeedFloat = this.windSpeed
-    const windSpeedInt = Math.round(windSpeedFloat)
-    windSpeed.textContent = "Wind Speed: " + windSpeedInt + "mph"
+      let windSpeed = document.querySelector(".wind-speed")
+      const windSpeedFloat = this.windSpeed
+      const windSpeedInt = Math.round(windSpeedFloat)
+      windSpeed.textContent = "Wind Speed: " + windSpeedInt + "meter/sec"
 
-    let windGust = document.querySelector(".wind-gust")
-      if (this.windGust) {
-        const windGustFloat = this.windGust
-        const windGustInt = Math.round(windGustFloat)
-        windGust.textContent = "Wind Gust: " + windGustInt + "mph"
-      } else {
-        windGust.textContent = "No Wind Gust"
-      }
+      let windGust = document.querySelector(".wind-gust")
+        if (this.windGust) {
+          const windGustFloat = this.windGust
+          const windGustInt = Math.round(windGustFloat)
+          windGust.textContent = "Wind Gust: " + windGustInt + "meter/sec"
+        } else {
+          windGust.textContent = "No Wind Gust"
+        }
+
+    } else if (settingsArray[1]=== "imperial") {
+
+      let icon = document.querySelector("#icon")
+      const iconImage = this.icon
+      icon.setAttribute("src", `http://openweathermap.org/img/wn/${iconImage}@2x.png`)
+
+      let city = document.querySelector(".city")
+      const cityName = this.city
+      city.textContent = cityName
+
+      let temperature = document.querySelector(".temperature")
+      const temperatureFloat = this.temperature
+      const temperatureInt = Math.round(temperatureFloat)
+      temperature.textContent = temperatureInt + " °f"
+
+      const feelsLike = document.querySelector(".feels-like")
+      const feelsLikeFloat = this.feelsLike
+      const feelsLikeInt = Math.round(feelsLikeFloat)
+      feelsLike.textContent = "Feels Like: " + feelsLikeInt + " °f"
+
+      const description = document.querySelector(".description")
+      const descriptionApi = this.description
+      description.textContent = descriptionApi
+
+      let windDirection = document.querySelector(".wind-direction")
+      const windDirectionFloat = this.windDirection
+      windDirection.textContent = "Wind Direction: " + windDirectionFloat + "°"
+
+      let cardinalDirection = document.querySelector(".cardinal-direction")
+      const nsew = this.degToCompass(windDirectionFloat)
+      cardinalDirection.textContent = nsew
+
+      let windSpeed = document.querySelector(".wind-speed")
+      const windSpeedFloat = this.windSpeed
+      const windSpeedInt = Math.round(windSpeedFloat)
+      windSpeed.textContent = "Wind Speed: " + windSpeedInt + "mph"
+
+      let windGust = document.querySelector(".wind-gust")
+        if (this.windGust) {
+          const windGustFloat = this.windGust
+          const windGustInt = Math.round(windGustFloat)
+          windGust.textContent = "Wind Gust: " + windGustInt + "mph"
+        } else {
+          windGust.textContent = "No Wind Gust"
+        }
+
+    }
 
   }
 }
